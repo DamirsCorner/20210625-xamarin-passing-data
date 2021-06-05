3a9e175e64ab4c429ebf0c021a009c40f5e250d3
Damir Arh <damir.arh@gmail.com>
2021-06-05 10:36:13 +0200

    Add project files.

---
+++ PassingDataToViewModel/PassingDataToViewModel/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using PassingDataToViewModel.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace PassingDataToViewModel.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ PassingDataToViewModel/PassingDataToViewModel/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PassingDataToViewModel.Models;
+using PassingDataToViewModel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PassingDataToViewModel.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
